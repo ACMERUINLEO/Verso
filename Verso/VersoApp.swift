358a2a33a1530a95a4de7917de39db50cf4078d4
Prolugue <acmeruin@gmail.com>
2026-07-22 14:38:48 +0800

    Initial Commit

---
+++ Verso/VersoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VersoApp.swift
+//  Verso
+//
+//  Created by Leo Chen on 2026/7/22.
+//
+
+import SwiftUI
+
+@main
+struct VersoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
